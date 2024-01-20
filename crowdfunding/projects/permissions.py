7289596cf286991ed3f8a9704769de9440dbe573
lindi <lindi.harding@gmail.com>
2024-01-20 09:38:03 +0800

    permissions for pledges and supporters

--- crowdfunding/projects/permissions.py
+++ crowdfunding/projects/permissions.py
@@ -5,3 +5,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+    
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.supporter == request.user
+    
+    # safe methods are effectively a get request, but check if the owner is the request user
