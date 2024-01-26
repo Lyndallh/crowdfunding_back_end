1959fdcd96e2fe791021b96867eb824a7581aea3
lindi <lindi.harding@gmail.com>
2024-01-26 14:13:37 +0800

    debugging passwrd hash issue

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -7,8 +7,17 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields ='__all__' 
         extra_kwargs = {'password': {'write_only': True}}
         
-        def create(self, validated_data):
-            return CustomUser.objects.create_user(**validated_data)
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+    
+class CustomUserDetailSerializer(CustomUserSerializer):
+    
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        return instance
         
 # This serializer looks a lot like our others, 
 # but there's a couple of things going on herethat are unique to users!
