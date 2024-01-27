7ef42bb7e150485b781abdd5f878a8b86412d854
lindi <lindi.harding@gmail.com>
2024-01-27 12:54:02 +0800

    remove date_created and implement date_modified timestamp in project update serializer

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Project, Pledge
+from datetime import datetime
 
 class PledgeSerializer(serializers.ModelSerializer):
     supporter = serializers.ReadOnlyField(source='supporter.id')
@@ -17,12 +18,19 @@ class ProjectDetailSerializer(ProjectSerializer):
     
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
-        # instance.description = "BLAH"
+        # Hard code test line
+            # instance.description = "BLAH"
         instance.description = validated_data.get('description', instance.description)
         instance.goal = validated_data.get('goal', instance.goal)
         instance.image = validated_data.get('image', instance.image)
         instance.is_open = validated_data.get('is_open', instance.is_open)
-        instance.date_created = validated_data.get('date_created', instance.date_created)
+
+        # Commented out original to keep original created date
+            # instance.date_created = validated_data.get('date_created', instance.date_created)
+        
+        # Update the date_modified with timestamp        
+        instance.date_modified = datetime.now()
+        
         instance.owner = validated_data.get('owner', instance.owner)
         instance.save()
         return instance
